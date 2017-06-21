79275752c4f015b81cf752e8ed27a3044ceab673
Erik Roberts <eriktheblue@gmail.com>
2017-06-21 13:19:01 -0400

    update getNumSuffix.m

--- getNumSuffix.m
+++ getNumSuffix.m
@@ -1,4 +1,10 @@
 function suffix = getNumSuffix(str)
-  suffix = regexp(str, '\D(\d*)$', 'tokens');
-  suffix = suffix{1}{1};
+% getNumSuffix - get numeric suffix from string; returns [] if no numeric suffix.
+
+suffix = regexp(str, '\D(\d*)$', 'tokens');
+suffix = str2double(suffix{1}{1});
+if isnan(suffix)
+  suffix = [];
+end
+
 end
