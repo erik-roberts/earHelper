5fee2e1db6faf56cca2cf8d46720ce8c5a726eda
Erik Roberts <eriktheblue@gmail.com>
2018-02-14 11:50:09 -0500

    use glob for lscell recursive

--- lscell.m
+++ lscell.m
@@ -1,4 +1,4 @@
-function dirList = lscell(arg, removePathBool, recursiveBool, relativePathBool)
+function dirList = lscell(arg, removePathBool, relativePathBool)
 %% lscell
 % Author: Erik Roberts
 %
@@ -7,93 +7,56 @@ function dirList = lscell(arg, removePathBool, recursiveBool, relativePathBool)
 % Usage: dirList = lscell()
 %        dirList = lscell(arg)
 %        dirList = lscell(arg, removePathBool)
+%        dirList = lscell(arg, removePathBool, relativePathBool)
 %
 % Inputs (optional):
 %   arg: argument to dir
 %   removePathBool: logical whether to remove the path before the files/dirs (default=true)
-%   recursiveBool: logical whether to find contents of subdirectories (default=false)
 %   relativePathBool: logical whether to convert absolute paths to relative paths (default=false)
+%
+% Tips: in order to search subdirectories, use the '**' glob character in the arg
+%
+% See also: DIR
 
-% args
-if nargin < 2 || isempty(removePathBool)
-  removePathBool = true; %defaults to true
-end
-if nargin < 3 || isempty(recursiveBool)
-  recursiveBool = false; %defaults to false
-end
-if nargin < 4 || isempty(relativePathBool)
-  relativePathBool = false; %defaults to false
-end
+% parse args
 if ~nargin || isempty(arg)
   arg = '.';
 end
-  
-if ~recursiveBool
-  try
-    dirListS = dir(arg);
-    
-    % remove first period
-    if strcmp(dirListS(1).name, '.')
-      dirListS(1) = [];
-    end
-    
-    % remove double period
-    if strcmp(dirListS(1).name, '..')
-      dirListS(1) = [];
-    end
-    
-    nFiles = length(dirListS);
-    
-    % convert struct to cellstr
-    dirList = cell(nFiles,1);
-    for k = 1:nFiles
-      dirList{k} = fullfile(dirListS(k).folder, dirListS(k).name);
-    end
-    
-    % dirList is cellstr with absolute paths
-    
-    if relativePathBool && ~removePathBool
-      dirList = regexprep(dirList, ['^' pwd filesep], '');
-    end
-    
-    % dirList is cellstr with absolute paths, or relative paths starting with name
-  catch % no arg files found
-    dirList = {};
-    return
-  end
-else % recursiveBool
-  if (ismac || isunix)
-    [~, dirList] = system(['find ' arg]);
-    dirList = strsplit(dirList, '[^ \S]', 'DelimiterType', 'RegularExpression');
-    dirList = dirList(:); % make col
-    
-    % remove first period
-    if strcmp(dirList{1}, '.')
-      dirList(1) = [];
-    end
-    
-    % remove trailing empty
-    if isempty(dirList{end})
-      dirList(end) = [];
-    end
-    
-    % dirList is cellstr with relative paths starting with './'
-    
-    % fix paths
-    if relativePathBool && ~removePathBool
-      % remove relative path starting './'
-      dirList = regexprep(dirList, ['^.' filesep], '');
-      
-    elseif ~relativePathBool && ~removePathBool
-      % convert relative paths to abs paths
-      dirList = regexprep(dirList, ['^.' filesep], [pwd filesep]);
-    end
-    
-    % dirList is cellstr with absolute paths, or relative paths starting with name
-  elseif ispc
-    warning('Recursive not implemented for windows.')
-  end
+if nargin < 2 || isempty(removePathBool)
+  removePathBool = true; %defaults to true
 end
+if nargin < 3 || isempty(relativePathBool)
+  relativePathBool = false; %defaults to false
+end
+
+% get dir contents
+dirListS = dir(arg);
+
+% remove first period
+if strcmp(dirListS(1).name, '.')
+  dirListS(1) = [];
+end
+
+% remove double period
+if strcmp(dirListS(1).name, '..')
+  dirListS(1) = [];
+end
+
+nFiles = length(dirListS);
+
+% convert struct to cellstr
+dirList = cell(nFiles,1);
+for k = 1:nFiles
+  dirList{k} = fullfile(dirListS(k).folder, dirListS(k).name);
+end
+
+% dirList is cellstr with absolute paths
+
+if relativePathBool && ~removePathBool
+  dirList = regexprep(dirList, ['^' pwd filesep], '');
+end
+
+% dirList is cellstr with absolute paths, or relative paths starting with name
 
 if removePathBool
   dirList = cellfun(@removePath, dirList, 'Uni',0);
