96039d6421de2538b11bda535cf711685e8c1f3c
Erik Roberts <eriktheblue@gmail.com>
2018-03-13 17:53:31 -0400

    new fn

--- isnan2.m
+++ isnan2.m
@@ -1,7 +1,7 @@
 function OUT = isnan2(IN)
-% isnan2 - same as isnan but returns false if ischar
+% isnan2 - same as isnan but returns false if ischar or iscategorical
 
-if ~ischar(IN) && isnan(IN)
+if ~ischar(IN) && ~iscategorical(IN) && isnan(IN)
   OUT = true;
 else
   OUT = false;
